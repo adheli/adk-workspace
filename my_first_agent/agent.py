7be9f730d75cd11ff312757edbb1cb719cbfe404
Eli Tavares <adheli.tavares@zinkworks.com>
2026-03-11 08:57:39 +0000

    ADK optmization

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -2,7 +2,7 @@ from google.adk.agents.llm_agent import Agent
 
 root_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    name='assistant_agent',
+    description='A helpful assistant',
+    instruction='You are a helpful assistant.'
 )
